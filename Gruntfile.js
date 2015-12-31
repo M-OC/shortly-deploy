9c3724edf98a6bd42efa7b0eb9c43e69cf4a4b64
Hack Reactor Students <accounts@hackreactor.com>
2015-12-30 19:13:56 -0800

    TEST

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,7 +125,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('deploy', [
     // add your deploy tasks here
-    'concat', 'uglify', 'nodemon'
+    'concat', 'uglify'
   ]);
 
 };
--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: test -3 ~/.bash_profile && echo "Found" || "Not Found"
 web: grunt deploy
